7243f8a7916b32035d11d5edbf69b1c635c72abb
fadlydestriana <fadlydestriana@gmail.com>
2024-06-05 11:18:55 +0700

    add home page to the code base

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tmdb_app/features/home/presentation/pages/all_page.dart';
+import 'package:tmdb_app/features/home/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
